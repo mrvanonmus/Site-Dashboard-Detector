66c3c6f1df449c7cebe688c4a103fecd4673556f
Jarjanazi <64444222+mrvanonmus@users.noreply.github.com>
2021-03-24 14:16:45 +0000

    Add files via upload

---
+++ 1.rb
@@ -0,0 +1,10 @@
+system ("clear") 
+
+puts ("\x1b[1;32m*▇▇▏◥▇◣┊◢▇◤▕▇▇*") 
+puts ( "\x1b[1;32m*▇▇▏▃▆▅▎▅▆▃▕▇▇*") 
+puts ("\x1b[1;32m*▇▇▏╱▔▕▎▔▔╲▕▇▇*") 
+puts ( "\x1b[1;32m*▇▇◣◣▃▅▎▅▃◢◢▇▇*") 
+puts ("\x1b[1;32m*▇▇▇◣◥▅▅▅◤◢▇▇▇*") 
+puts ("\x1b[1;32m*▇▇▇▇◣╲▇╱◢▇▇▇▇*") 
+system ("chmod 777 SDD.py")
+system ("python2 SDD.py") 
